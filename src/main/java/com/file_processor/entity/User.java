8f9347dca2bf4321f763b142007fdac31b1c1ed5
saikrishna4343 <saikrishnapvn@gmail.com>
2024-09-01 21:25:55 -0500

    Base code

---
+++ src/main/java/com/file_processor/entity/User.java
@@ -0,0 +1,25 @@
+package com.file_processor.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String userId;
+}
